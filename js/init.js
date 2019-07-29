dc333051389825e56476aa369c875ca21a905244
Maruf H <30820028+MH-fci@users.noreply.github.com>
2019-07-30 00:42:38 +0600

    Update init.js
    change name ui/ux

--- js/init.js
+++ js/init.js
@@ -509,7 +509,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["Freelancer", "UI/UX Designer", "Web Developer"],
+			strings: ["Freelancer", "Software Tester", "Web Developer"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
