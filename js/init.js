8aac7c249497deec3513dd961e054fca5d8cfda1
Maruf H <30820028+mh-fci@users.noreply.github.com>
2020-12-14 21:30:59 +0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -395,7 +395,7 @@ function arlo_tm_owl_carousel(){
 
  new WOW().init();
 
-// -----------------------------------------------------
+// ----------------------------------------------------
 // -----------------    PROGRESS BAR    ----------------
 // -----------------------------------------------------
 
@@ -509,7 +509,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["Freelancer", "Software Tester", "Web Developer"],
+			strings: ["Web Developer"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
